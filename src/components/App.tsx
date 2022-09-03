0f8c4de6b95db9e579b150054077910febb47a72
Im-Rises <quentin-morel88@hotmail.com>
2022-09-03 01:26:58 -0400

    Added webcam creation and image input handling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from '../img/logo.svg';
+import * as tf from '@tensorflow/tfjs';
+import Webcam from 'react-webcam';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-          Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-          Learn React
-				</a>
-			</header>
+			<input type='file' multiple accept='image/*' onChange={onImageChange} />
+			<canvas/>
+			<Webcam audio={false} style={{display: 'none'}}/>
 		</div>
 	);
+}
+
+function onImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+	const {files} = event.target;
+	if (files === null) {
+		return;
+	}
+
+	const file = files[0];
+	const image = document.createElement('img');
+	image.src = URL.createObjectURL(file);
+	image.onload = () => {
+		const canvas = document.querySelector('canvas')!;
+		canvas.width = image.width;
+		canvas.height = image.height;
+		const context = canvas.getContext('2d')!;
+		context.drawImage(image, 0, 0);
+		const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+		// Const data = imageData.data;
+		const tensor = tf.browser.fromPixels(imageData, 4);
+		tensor.print();
+	};
 }
 
 export default App;
